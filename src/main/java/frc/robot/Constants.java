f92b588e00363cc3d0bf1b4f320728652d93a88b
IsaacM75 <isaacmackenzie75@gmail.com>
2025-01-29 16:51:36 -0500

    Swerve base updated to 2025 with working creep drive.

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,53 @@
+package frc.robot;
+
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.XboxController;
+
+public final class Constants {
+  public final class DriveBase {
+    public static final double maxSpeed = Units.feetToMeters(14.5); // metres per second
+    // TODO: why this number?
+    public static final double maxAngularVelocity = 5.627209491911525; // radians per second
+    public static final double maxCreepSpeed = Units.feetToMeters(2); // metres per second
+    public static final double maxCreepAngularVelocity = 2; // radians per second
+    public static final double wheelDiameter = 4; // inches
+    public static final double driveGearRatio = 6.75; // revolutions per wheel rotation
+    // TODO: how to get is this number?
+    public static final double angleGearRatio = 21.4285714286;
+    // TODO: what is this?
+    public static final double anglePulsesPerRotation = 360;
+
+    // TODO: tune these
+    public final class TranslationPID {
+      public static final double p = 0.7;
+      public static final double i = 0;
+      public static final double d = 0;
+    }
+
+    // TODO: tune these
+    public final class RotationPID {
+      public static final double p = 0.4;
+      public static final double i = 0;
+      public static final double d = 0;
+    }
+  }
+
+  public final class Field {
+    private final AprilTagFieldLayout aprilTagFieldLayout =
+        AprilTagFieldLayout.loadField(AprilTagFields.k2025Reefscape);
+  }
+
+  public final class Control {
+    public final class Main {
+      public static final XboxController controller = new XboxController(0);
+      public static final double leftXDeadband = 0.1;
+      public static final double leftYDeadband = 0.1;
+      public static final double rightXDeadband = 0.1;
+      public static final double rightYDeadband = 0.1;
+      public static final int zeroSwerveDriveButton = XboxController.Button.kA.value;
+      public static final int enableCreepDrive = XboxController.Axis.kLeftTrigger.value;
+    }
+  }
+}
---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,61 @@
+package frc.robot;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.event.BooleanEvent;
+import edu.wpi.first.wpilibj.event.EventLoop;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Subsystems.*;
+import java.io.File;
+
+public class RobotContainer {
+  private static RobotContainer instance;
+  private XboxController mainController = Constants.Control.Main.controller;
+  private SwerveSubsystem drivebase;
+  private Command driveFieldOrientedDirectAngle;
+
+  private EventLoop loop = new EventLoop();
+
+  private RobotContainer() {
+    drivebase = new SwerveSubsystem(new File(Filesystem.getDeployDirectory(), "swerve/neo"));
+
+    driveFieldOrientedDirectAngle =
+        drivebase.driveCommand(
+            () ->
+                MathUtil.applyDeadband(
+                    -mainController.getLeftY(), Constants.Control.Main.leftYDeadband),
+            () ->
+                MathUtil.applyDeadband(
+                    -mainController.getLeftX(), Constants.Control.Main.leftXDeadband),
+            () -> -mainController.getRightX(),
+            () -> -mainController.getRightY());
+
+    configureBindings();
+    System.out.println("ROBOT CONTAINER CREATED");
+  }
+
+  public static RobotContainer getInstance() {
+    if (instance == null) instance = new RobotContainer();
+    return instance;
+  }
+
+  private void configureBindings() {
+
+    BooleanEvent zeroDriveBase =
+        new BooleanEvent(
+            loop, () -> mainController.getRawButton(Constants.Control.Main.zeroSwerveDriveButton));
+    zeroDriveBase.rising().ifHigh(() -> drivebase.zeroGyro());
+
+    BooleanEvent enableCreepDrive =
+        new BooleanEvent(
+            loop, () -> mainController.getRawAxis(Constants.Control.Main.enableCreepDrive) > 0.5);
+    enableCreepDrive.rising().ifHigh(() -> drivebase.setCreepDrive(true));
+    enableCreepDrive.falling().ifHigh(() -> drivebase.setCreepDrive(false));
+    drivebase.setDefaultCommand(driveFieldOrientedDirectAngle);
+  }
+
+  public void teleopPeriodic() {
+    loop.poll();
+  }
+}
